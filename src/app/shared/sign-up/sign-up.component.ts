0fc219d6e23b79c2c3b11ff703781196d479e2a4
rifsh <rifshmuhammed@gmail.com>
2024-05-10 09:43:47 +0530

    founder signup and payment

---
+++ src/app/shared/sign-up/sign-up.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  @ViewChild('signUpForm') signUpFormValues: NgForm;
+
+  selectedImage: string | null = null;
+  file: File = null;
+
+  constructor(private authSrvc: AuthService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  selectFile(event) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      reader.onload = (e: any) => {
+        this.selectedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+
+    this.signUpFormValues.value.image = event.target.files[0];
+    if (event.target.files.length > 0) {
+      this.file = <File>event.target.files[0];
+      this.signUpFormValues.value.image = this.file;
+    }
+  }
+
+  signUp() {
+    this.authSrvc.founderSignup(this.signUpFormValues, this.file).subscribe((res) => {
+      console.log(res);
+      alert('Successfully registered');
+    }, (err) => {
+      console.log(err);
+      alert('An error Occured');
+    })
+  }
+
+}
